0e0bfc95c669e432d114c4687d323644ebd2686f
NicholasDHarris <nicholas.harris@maine.edu>
2021-04-30 13:30:08 -0400

    Step 5.5: Hello to Ester.

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,9 +6,10 @@ public class HelloWorld {
 		System.out.println("Hello World!");
 		System.out.println("Ciao, tutti!");
 		System.out.println("\nHello, Alice.");		
-		System.out.println("\nHello, Bob.");
-		System.out.println("\nHello, Charlie.");
-		System.out.println("\nHello, Denise.");
+		System.out.println("Hello, Bob.");
+		System.out.println("Hello, Charlie.");
+		System.out.println("Hello, Denise.");
+		System.out.println("Hello, Ester.");
 	}
 
 }
