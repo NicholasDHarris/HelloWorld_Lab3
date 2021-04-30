3efcacc6d8a57bdc76196bf67526319a1296d1fd
NicholasDHarris <nicholas.harris@maine.edu>
2021-04-30 12:48:00 -0400

    Lab 2 material. Step 2.2: Added googdbye message.

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hear ye: important announcement");
 		System.out.println("Hello World!");
+		System.out.println("Ciao, tutti!");
 
 	}
 
