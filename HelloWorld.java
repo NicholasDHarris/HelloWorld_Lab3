0c8a643ef500062cbafd56f6705958ef349b4f4a
NicholasDHarris <nicholas.harris@maine.edu>
2021-04-30 12:55:11 -0400

    Lab 2 material. Step 3.3: Hello to Alice.

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 		System.out.println("Hear ye: important announcement");
 		System.out.println("Hello World!");
 		System.out.println("Ciao, tutti!");
+		System.out.println("\nHello, Alice.");
 
 	}
 
